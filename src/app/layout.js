058c91088a5ca5bde443592bc259fe9d46456ee4
Muslim <max2dgame12@gmail.com>
2025-12-26 20:18:31 +0300

    add google analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,17 @@
 import { Cairo, Tajawal } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const cairo = Cairo({
   subsets: ["arabic"],
   weight: ["400", "700"],
-  variable: "--font-cairo", 
+  variable: "--font-cairo",
 });
 
 const tajawal = Tajawal({
   subsets: ["arabic"],
   weight: ["400", "500", "700"],
-  variable: "--font-tajawal", 
+  variable: "--font-tajawal",
 });
 
 
@@ -22,7 +23,21 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className=" scroll-smooth">
+    <html lang="en" className=" scroll-smooth">{/* Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-1MTZXGJFRS"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-1MTZXGJFRS', {
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
       <body
         className={`${cairo.variable} ${tajawal.variable} font-cairo antialiased`}
       >
