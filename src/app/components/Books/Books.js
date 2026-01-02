9d3de551b9448b43e53241ef8ff582c9a7df9f12
Muslim <max2dgame12@gmail.com>
2026-01-02 12:52:01 +0300

    some fix

--- src/app/components/Books/Books.js
+++ src/app/components/Books/Books.js
@@ -10,7 +10,7 @@ const Books = () => {
             <div className='container flex-col'>
                 <div className='w-full flex flex-col items-center justify-center text-center !pb-7 !gap-3'>
                     <h1 className='text-4xl'>كتب الدراسة الحوزوية</h1>
-                    <p className='texg-xl  text-neutral-600'>مرتبة حسب المراحل وفق منهج واضح ويسير لطلبة العلم</p>
+                    <p className='texg-xl  text-neutral-600'>يعرض موقع مكتبة الحوزة العلمية الكتب مرتبة حسب المراحل وفق منهج واضح ويسير لطلبة العلم</p>
                 </div>
                 <After/>
                 <Almuqadimat/>
